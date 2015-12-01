1fb42ceeb04f22f6bf54f1434dc93311c2d91e2c
cr6588 <cr6588@vip.qq.com>
2015-12-01 09:33:05 +0800

    java 原始数据类型数组初始值

--- src/test/java/test/BaseDataTypeInitValueTest.java
+++ src/test/java/test/BaseDataTypeInitValueTest.java
@@ -6,10 +6,16 @@ public class BaseDataTypeInitValueTest {
 
     @Test
     public void test() {
-        short s = 0;int i;long l; byte b; char c; boolean bo; float f; double d;
-        System.out.println("short init value is " + s);
-        String str = new String("fd");
-        str.equals("s");
+        short s[] = new short[1];int i[] = new int[1];long l[] = new long[1]; byte b[] = new byte[1]; char c[] = new char[1]; boolean bo[] = new boolean [1]; float f[] = new float[1]; double d[] = new double[1];
+        System.out.println("short init value is " + s[0]);
+        System.out.println("int init value is " + i[0]);
+        System.out.println("long init value is " + l[0]);
+        System.out.println("byte init value is " + b[0]);
+        System.out.println("char init value is " + c[0]);
+        System.out.println("boolean init value is " + bo[0]);
+        System.out.println("float init value is " + f[0]);
+        System.out.println("double init value is " + d[0]);
+        System.out.println(new String("s").equals(new String("s")));
     }
 
 }
