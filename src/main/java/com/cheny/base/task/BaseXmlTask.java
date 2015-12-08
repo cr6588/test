511daa670312a63052a5bb21a36f6258a12b99fe
Ruter <Ruter@Ruter-PC>
2015-12-08 19:17:56 +0800

    add task

---
+++ src/main/java/com/cheny/base/task/BaseXmlTask.java
@@ -0,0 +1,15 @@
+package com.cheny.base.task;
+
+import java.text.SimpleDateFormat;
+
+public class BaseXmlTask {
+
+    public static boolean main = true;
+
+    public void task() {
+        if (!main) {
+            return;
+        }
+        System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(System.currentTimeMillis()) + ":xml 定时任务");
+    }
+}
---
+++ src/main/java/com/cheny/base/task/SpringTask.java
@@ -0,0 +1,45 @@
+package com.cheny.base.task;
+
+import java.text.SimpleDateFormat;
+
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@PropertySource("classpath:task.properties")
+@Component
+public class SpringTask implements Runnable{
+
+//    @Autowired
+//    private Environment env;
+
+    @Scheduled(fixedRate=2000) 
+    public void outTaskTest() throws InterruptedException {
+//        System.out.println(env.getProperty("task").toString());
+        System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(System.currentTimeMillis()) + "fixedRate测试");
+        Thread.sleep(4000);
+        System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(System.currentTimeMillis()) + "fixedRate定时恢复");
+    }
+
+    @Scheduled(fixedDelay=2000) 
+    public void fixedDelayTaskTest() throws InterruptedException {
+//        System.out.println(env.getProperty("task").toString());
+        System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(System.currentTimeMillis()) + "fixedDelay测试");
+        Thread.sleep(4000);
+        System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(System.currentTimeMillis()) + "fixedDelay定时恢复");
+    }
+
+    @Scheduled(cron="0/5 * * * * ?")
+    public void cronTaskTest() throws InterruptedException {
+//        System.out.println(env.getProperty("task").toString());
+        System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(System.currentTimeMillis()) + "cron测试");
+        Thread.sleep(4000);
+        System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(System.currentTimeMillis()) + "cron定时恢复");
+    }
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        
+    }
+}
